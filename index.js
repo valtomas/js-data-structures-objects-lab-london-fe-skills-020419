d752e3691929dc902af4c59c61ba58eeda41fd7c
Tomas Val <tomas.val@wework.com>
2019-03-11 20:40:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ var driver = {
 }
 
 function updateDriverWithKeyAndValue(driver, key, value) {
- var newdriver = driver ;
- Object.assign = (driver, {key: value}) 
-  
+  Object.assign = (driver, {key: value}) 
 }
 
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   
- Object.assign = (driver, {key: value}) 
+  Object.assign = (driver, {key: value}) 
   
 }
